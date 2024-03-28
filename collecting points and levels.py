1a8e80bf180c5437b82ed85bb7be4b216176f597
WilsonD09 <158211754+WilsonD09@users.noreply.github.com>
2024-03-28 14:26:16 -0400

    Update collecting points and levels.py

--- collecting points and levels.py
+++ collecting points and levels.py
@@ -4,14 +4,15 @@ import os
 with open("player.json", "r") as f:
     player = json.load(f)
 
-x = input('What is your username? ')
-login = ('unsuccessful')
-while login == ('unsuccessful'):
-    for i in player:
-        if x in i['user']:
-            print('Logged in ')
-            login = ('successful')
-        else:
-            x = input('Username does not exist. Please enter again ')
-            login = ('unsuccessful')
-
+class Play():
+    def login():
+        x = input('What is your username? ')
+        login = ('unsuccessful')
+        while login == ('unsuccessful'):
+            for i in player:
+                if x in i['user']:
+                    print('Logged in ')
+                    login = ('successful')
+                else:
+                    x = input('Username does not exist. Please enter again ')
+                    login = ('unsuccessful')
