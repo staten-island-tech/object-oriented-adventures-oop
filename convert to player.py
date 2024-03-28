75dc386fbb89636ff25f56fb569171c6401b600d
WilsonD09 <158211754+WilsonD09@users.noreply.github.com>
2024-03-28 13:55:45 -0400

    made conversion from user to player

--- convert to player.py
+++ convert to player.py
@@ -1,8 +1,11 @@
 import json
+import os
 
-test = open("data.json", encoding="utf8")
-
+test = open("user.json", encoding="utf8")
 user = json.load(test)
+
+with open("player.json", "r") as f:
+    player = json.load(f)
 
 x = input('What is your username? ')
 login = ('unsuccessful')
@@ -11,9 +14,25 @@ while login == ('unsuccessful'):
         print('Logged in ')
         login = ('successful')
     else:
-        input('Username does not exist. Please enter again ')
+        x = input('Username does not exist. Please enter again ')
         login = ('unsuccessful')
 
-class convert():
-    def convert():
-        player = {}
+class Convert():
+    def __init__(data,user,points):
+        data.user = user
+        data.points = points
+    def __str__(data):
+        return f"{data.user},{data.points}"
+def convert(user,points):
+    new_player = Convert(user,points)
+    player.append(new_player.__dict__)
+
+convert(x,0)
+    
+new_file = "updated.json"
+with open(new_file, "w") as f:
+    json_string = json.dumps(player)
+    f.write(json_string)
+
+os.remove("player.json")
+os.rename(new_file, "player.json")
