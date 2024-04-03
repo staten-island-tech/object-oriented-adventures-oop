2b42b6768be77c77a1418228331c06f974b5524b
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2024-04-03 14:00:01 -0400

    ss

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def sprint(str):
    for c in str + '\n':
      sys.stdout.write(c)
      sys.stdout.flush()
-     time.sleep(3./90)
+     time.sleep(2./90)
 
 ##giving the story line of the game
 sprint('Hello welcome to the Matthew and Wilson game!')
@@ -13,10 +13,9 @@ sprint('The choises are Mage, Warrior, Tank, and Ace.')
 sprint('Mages have increased magic stats and magic powers.')
 sprint('Warriors have increased strenght and attack power')
 sprint('Tanks have increased health and defensive stats')
-sprint('Aces have increased speed and a gradual increase in every other stat')
-x = input(sprint('Please chose'))
+sprint('Aces have increased speed and a slight increase in every other stat')
 
-
+import player
 """ x = input('do you want to start a new game? [Y/N]:') ##login code to the game
 if x == ('Y').upper():
     import username """
