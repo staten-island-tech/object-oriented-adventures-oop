7754e2b3f5e2fa57a1da24c1ebe29ee9b7215256
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2024-04-03 13:54:51 -0400

    z

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-x = input('Do you have a username? [Yes/No] ')
-if x == 'Yes':
-    import username
-else import
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(3./90)
+
+##giving the story line of the game
+sprint('Hello welcome to the Matthew and Wilson game!')
+sprint('Welcome to Willtopia! This is where you will start your monster slaying journey.')
+sprint('You will have a choice Between mulitpule classes.')
+sprint('The choises are Mage, Warrior, Tank, and Ace.')
+sprint('Mages have increased magic stats and magic powers.')
+sprint('Warriors have increased strenght and attack power')
+sprint('Tanks have increased health and defensive stats')
+sprint('')
+x = input(sprint('Please chose'))
+
+
+""" x = input('do you want to start a new game? [Y/N]:') ##login code to the game
+if x == ('Y').upper():
+    import username """
