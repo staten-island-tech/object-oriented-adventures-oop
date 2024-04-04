d27bee749ba3a33e0f10ee9483fe35b5d718d5fb
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2024-04-04 14:00:56 -0400

    zz

--- functions.py
+++ functions.py
@@ -1,5 +1,4 @@
 import sys,time
-import 
 
 class Text():
     
@@ -12,22 +11,5 @@ class Text():
     def delete():
         sys.stdout.write('\x1b[1A')
         sys.stdout.write('\x1b[2K')
-    
 
-Text.sprint('Loading game ...')
-Text.delete()
-Text.sprint('Loading game ...')
-Text.delete()
-Text.sprint('Loading game ...')
-Text.delete()
-Text.sprint('Loading game ...')
-Text.delete()
-Text.sprint('Loading game ...')
-Text.delete()
-Text.sprint('Welcome to Pythonia! This is where you will start your monster slaying journey.')
-Text.sprint('You will have a choice Between mulitpule classes.')
-Text.sprint('The choises are Mage, Warrior, Tank, and Ace.')
-Text.sprint('Mages have increased magic stats and magic powers.')
-Text.sprint('Warriors have increased strenght and attack power')
-Text.sprint('Tanks have increased health and defensive stats')
-Text.sprint('Aces have increased speed and a slight increase in every other stat')
+    
