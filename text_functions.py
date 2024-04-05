1de23b5132b813294170618df455eb595ec3bfda
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2024-04-05 14:00:24 -0400

    aaa

--- text_functions.py
+++ text_functions.py
@@ -2,11 +2,17 @@ import sys,time
 
 class Text():
     
-    def sprint(str):
+    def slow_print(str):
         for c in str + '\n':
             sys.stdout.write(c)
             sys.stdout.flush()
-            time.sleep(2./90)
+            time.sleep(4./90)
+
+    def fast_print(str):
+        for c in str + '\n':
+            sys.stdout.write(c)
+            sys.stdout.flush()
+            time.sleep(1./90)
     
     def delete():
         sys.stdout.write('\x1b[1A')
