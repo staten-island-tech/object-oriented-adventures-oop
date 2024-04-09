f332382acb6740eb5b4d148c58573990477885b4
WilsonD09 <158211754+WilsonD09@users.noreply.github.com>
2024-04-09 12:45:46 -0400

    Squashed commit of the following:
    commit 411a462b9df5f197298368b3e4e68d46c842781f
    Author: WilsonD09 <158211754+WilsonD09@users.noreply.github.com>
    Date:   Mon Apr 8 13:58:39 2024 -0400
    
        z
    
    commit 1de23b5132b813294170618df455eb595ec3bfda
    Author: Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
    Date:   Fri Apr 5 14:00:24 2024 -0400
    
        aaa
    
    commit 1ae4fb4fbebb077679fb6d36fb576680409729ef
    Author: Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
    Date:   Fri Apr 5 13:55:30 2024 -0400
    
        wilson is pretty cool
    
    commit d27bee749ba3a33e0f10ee9483fe35b5d718d5fb
    Author: Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
    Date:   Thu Apr 4 14:00:56 2024 -0400
    
        zz
    
    commit ab8d8a4a2ef9e2bcf7afcd03d07bccaa09ea8bc3
    Author: Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
    Date:   Thu Apr 4 13:48:11 2024 -0400
    
        f
    
    commit 1ab9e25302375a1dcfc878d03b1c53ffcb59f7ed
    Author: Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
    Date:   Thu Apr 4 13:41:05 2024 -0400
    
        zzz
    
    commit 2b42b6768be77c77a1418228331c06f974b5524b
    Author: Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
    Date:   Wed Apr 3 14:00:01 2024 -0400
    
        ss
    
    commit 2327289ac17d0d571bfbd65a3687f1b86ffcd2a6
    Merge: 9f46771 a6d7dd0
    Author: WilsonD09 <158211754+WilsonD09@users.noreply.github.com>
    Date:   Wed Apr 3 13:56:21 2024 -0400
    
        Merge branch 'classes'
    
    commit 9f467710563da12553c516f5123cfd05a7388cb5
    Author: Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
    Date:   Wed Apr 3 13:55:32 2024 -0400
    
        z
    
    commit 7754e2b3f5e2fa57a1da24c1ebe29ee9b7215256
    Author: Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
    Date:   Wed Apr 3 13:54:51 2024 -0400
    
        z
    
    commit f646af2186abec0f26266730008ad4d9fe0efebe
    Author: WilsonD09 <158211754+WilsonD09@users.noreply.github.com>
    Date:   Tue Apr 2 13:46:26 2024 -0400
    
        Update main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from text_functions import Text 
+
+Text.slow_print('Loading game ...')
+Text.delete()
+Text.slow_print('Loading game ...')
+Text.delete()
+Text.slow_print('Loading game ...')
+Text.delete()
+Text.slow_print('Loading game ...')
+Text.delete()
+Text.slow_print('Loading game ...')
+Text.delete()
+Text.fast_print('Welcome to Pythonia! This is where you will start your monster slaying journey.')
+Text.fast_print('You will have a choice Between mulitpule classes.')
+Text.fast_print('The choises are Mage, Warrior, Tank, and Ace.')
+Text.fast_print('Mages have increased magic stats and magic powers.')
+Text.fast_print('Warriors have increased strenght and attack power')
+Text.fast_print('Tanks have increased health and defensive stats')
+Text.fast_print('Aces have increased speed and a slight increase in every other stat')
+Text.fast_print('Please select your class next')
+Text.delete_all(8)
+
+x = input(Text.slow_print('Now that you have selected your class are you ready to start your tutorial? [Y/N]:'))
+if x == ('Y'):
+    Text.slow_print('hi')
---
+++ text_functions.py
@@ -0,0 +1,28 @@
+import sys,time
+
+class Text():
+    
+    def slow_print(str):
+        for c in str + '\n':
+            sys.stdout.write(c)
+            sys.stdout.flush()
+            time.sleep(4./90)
+
+    def fast_print(str):
+        for c in str + '\n':
+            sys.stdout.write(c)
+            sys.stdout.flush()
+            time.sleep(1./90)
+    
+    def delete():
+        sys.stdout.write('\x1b[1A')
+        sys.stdout.write('\x1b[2K')
+    
+    def delete_all(x):
+        i = 0
+        while i < x:
+            sys.stdout.write('\x1b[1A')
+            sys.stdout.write('\x1b[2K')
+            i += 1
+
+    
