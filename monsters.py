314badada65da50ab5b574cee130a460f589c727
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2024-04-11 13:57:47 -0400

    zz

--- monsters.py
+++ monsters.py
@@ -11,5 +11,9 @@ class Monster():
     def __str__(data):
         return f"{data.monster},{data.monster_level},{data.attack_strength},{data.health_level},{data.speed},{data.experince_dropped}"
     
-def Zombie():
-    super
+Monster('Zombie', 1, 1, 1, 1, .5)
+Monster('Skeleton', 2, 2, 1, 1, 1.5)
+Monster('Golem', 3, 3, 3, 1, 3)
+Monster('Ogre', 3, 2, 2, 2, 2.5)
+Monster('Gnome', 4, 1, 4, 4, 3.5)
+Monster()
