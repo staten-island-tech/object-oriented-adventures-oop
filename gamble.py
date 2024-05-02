89da1e32acec0d1bdeb5f94240033df164f8ba7b
WilsonD09 <158211754+WilsonD09@users.noreply.github.com>
2024-05-02 13:57:10 -0400

    Update gamble.py

--- gamble.py
+++ gamble.py
@@ -1,5 +1,12 @@
 import random
 import text_functions
+import json
+import os
+from classes import user
+
+with open("player.json", "r") as f:
+    player = json.load(f)
+
 
 t = text_functions.Text
 for load in range(3):
@@ -11,3 +18,10 @@ But be careful, you could lose it all anytime.
 Just remember 99 percent of gamblers stop before they win big.
 You can only lost 100 percent of your money, you can win 2000 percent of it back.''')
 t.delete_all(5)
+
+def Gamble(user):
+    for players in player:
+        if user == players['user']
+        print(f'Your Balance is {players['money']}')
+    x = input('How much money are you going to gamble?')
+
