3acb282b496cd7e717cf3007152dae04e29374be
WilsonD09 <158211754+WilsonD09@users.noreply.github.com>
2024-05-06 13:58:10 -0400

    Update fighting.py

--- fighting.py
+++ fighting.py
@@ -1,6 +1,7 @@
 import text_functions
 import json
 import os
+from classes import user
 
 with open("player.json", "r") as f:
     player = json.load(f)
@@ -8,3 +9,19 @@ with open("player.json", "r") as f:
 with open("monster.json", "r") as f:
     monster = json.load(f)
 
+text_functions.Load(3)
+t = text_functions.Text
+t.fast_print('''This is the fighting system.
+You can fight monsters to level up and gain money.
+These Monsters are strong, 
+I suggest training to be able to beat them.''')
+t.delete_all(4)
+
+cont = input('Do you wish to continue? Yes/No')
+if cont == ('Yes'):
+    print('Starting Encounter')
+
+def encounter(user):
+    for players in player:
+        if user == players['user']:
+            
