94a041065e19ef1e1ef3c1cf8cdfe8608fa4e924
WilsonD09 <158211754+WilsonD09@users.noreply.github.com>
2024-05-14 13:57:34 -0400

    Update fighting.py

--- fighting.py
+++ fighting.py
@@ -26,23 +26,30 @@ cont = input('Do you wish to continue? Yes/No ')
 if cont == ('Yes'):
     print('Starting Encounter')
 
-def encounter():
-    for players in player:
-        if user == players['user']:
-            power = int(players['levels'])
-            monsterpower = random.randrange(power-1,power+1)
-            m = monster[monsterpower]
-            mname = m['monster']
-            print(f'You have encountered a {mname}')
-            print(m)
-            return m
-def squareup():
-    m = encounter()
-    for players in player:
-        if user == players['user']:
-            [print(moves[i]) for i in range(0,int(players['levels']))]
-            move = input('What move would you like to use? ')
+class Fight():
+    def encounter():
+        for players in player:
+            if user == players['user']:
+                power = int(players['levels'])
+                monsterpower = random.randrange(power-1,power+1)
+                m = monster[monsterpower]
+                mname = m['monster']
+                print(f'You have encountered a {mname}')
+                return m
+    def playermove():
+        m = Fight.encounter()
+        print(m)
+        for players in player:
+            if user == players['user']:
+                for i in moves:
+                    if int(i['level']) <= int(players['levels']):
+                        print(i)
+                move = input('What move would you like to use? ')
+                for i in moves:
+                    if move == i['name']:
+                        hpleft = (int(m['health_level']) - int(i['power']))
+                        print(hpleft)
             
         
 
-squareup()
+Fight.playermove()
