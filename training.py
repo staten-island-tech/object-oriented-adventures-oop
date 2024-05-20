540c07b9a7d38934ec0462e267dc8f604fd95e97
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2024-05-20 14:01:51 -0400

    zzz

--- training.py
+++ training.py
@@ -12,45 +12,62 @@ t = text_functions.Text
 with open("player.json", mode='r') as read:
     data = json.load(read)
 
+x = user
+
 def Training():
     y = input(t.fast_print('Would you like to train? [Y/N]: ')).upper()
-    while y == 'Y':
+    t.delete_all(2)
+    if y == 'Y':
         t.fast_print('Training each skill takes 50 experince points and returns one point for that skill.')
-        for i in data:
-            if x == i['user']:
-                    x = input(t.fast_print('What do you want to train?: ')).lower().capitalize()
-                    if i['exp'] >= 50:
-                        if 'St' in x:
-                            i.update({'strength':int(i['strength'])+1})
-                            i.update({'exp':int(i['exp'])-50})
-                            time.sleep(.5)
-                            t.fast_print('Thank you for training strength, your stat boost has been applied.')
-                            time.sleep(.5)
-                            y = input(t.fast_print('Would you like to train again? [Y/N]: ')).lower().capitalize()
-                        elif 'D' in x:
-                            i.update({'defense':int(i['defense'])+1})
-                            i.update({'exp':int(i['exp'])-50})
-                            time.sleep(.5)
-                            t.fast_print('Thank you for training defense, your stat boost has been applied.')
-                            time.sleep(.5)
-                            y = input(t.fast_print('Would you like to train again? [Y/N]: ')).upper()
-                        elif 'M' in x:
-                            i.update({'magic':int(i['magic'])+1})
-                            i.update({'exp':int(i['exp'])-50})
-                            time.sleep(.5)
-                            t.fast_print('Thank you for training magic, your stat boost has been applied.')
-                            time.sleep(.5)
-                            y = input(t.fast_print('Would you like to train again? [Y/N]: ')).upper()
-                        elif 'Sp' in x:
-                            i.update({'speed':int(i['speed'])+1})
-                            i.update({'exp':int(i['exp'])-50})
-                            time.sleep(.5)
-                            t.fast_print('Thank you for training speed, your stat boost has been applied.')
-                            time.sleep(.5)
-                            y = input(t.fast_print('Would you like to train again? [Y/N]: ')).lower().capitalize()
-                    elif i['exp'] < 50:
-                        t.fast_print('You do not have enough experince points to train please come back later when you have more')
-                        time.sleep(1)
+        time.sleep(1.5)
+        t.delete_all(3)
+        while y == 'Y':
+            for i in data:
+                if x == i['user']:
+                        z = input(t.fast_print('What do you want to train?: ')).lower().capitalize()
+                        t.delete_all(2)
+                        if i['exp'] >= 50:
+                            if 'St' in z:
+                                i.update({'strength':int(i['strength'])+1})
+                                i.update({'exp':int(i['exp'])-50})
+                                time.sleep(.5)
+                                t.fast_print('Thank you for training strength, your stat boost has been applied.')
+                                time.sleep(1)
+                                t.delete_all(3)
+                                y = input(t.fast_print('Would you like to train again? [Y/N]: ')).lower().capitalize()
+                                t.delete()
+                            elif 'D' in z:
+                                i.update({'defense':int(i['defense'])+1})
+                                i.update({'exp':int(i['exp'])-50})
+                                time.sleep(.5)
+                                t.fast_print('Thank you for training defense, your stat boost has been applied.')
+                                time.sleep(1)
+                                t.delete_all(3)
+                                y = input(t.fast_print('Would you like to train again? [Y/N]: ')).upper()
+                                t.delete()
+                            elif 'M' in z:
+                                i.update({'magic':int(i['magic'])+1})
+                                i.update({'exp':int(i['exp'])-50})
+                                time.sleep(.5)
+                                t.fast_print('Thank you for training magic, your stat boost has been applied.')
+                                time.sleep(1)
+                                t.delete_all(3)
+                                y = input(t.fast_print('Would you like to train again? [Y/N]: ')).upper()
+                                t.delete()
+                            elif 'Sp' in z:
+                                i.update({'speed':int(i['speed'])+1})
+                                i.update({'exp':int(i['exp'])-50})
+                                time.sleep(.5)
+                                t.fast_print('Thank you for training speed, your stat boost has been applied.')
+                                time.sleep(1)
+                                t.delete_all(3)
+                                y = input(t.fast_print('Would you like to train again? [Y/N]: ')).lower().capitalize()
+                                t.delete()
+                        elif i['exp'] < 50:
+                            t.fast_print('You do not have enough experince points to train please come back later when you have more')
+                            time.sleep(1)
+                            t.delete_all(3)
+                            y = 'N'
 
 Training()
 
